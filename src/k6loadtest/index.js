4db3375f2aeab0bf79730d78f0855206d412bbe7
Nada Salsabila <99460188+nadahusin@users.noreply.github.com>
2022-10-08 15:49:28 +0700

    add unit testing and integration test

---
+++ src/k6loadtest/index.js
@@ -0,0 +1,27 @@
+const http = require('k6/http')
+const metrics = require('k6/metrics')
+const k6 = require('k6')
+
+const myFailRate = new metrics.Rate('failed request')
+
+module.exports.options = {
+    stages: [
+        { duration: '10s', target: 20 },
+        { duration: '20s', target: 100 },
+        { duration: '20s', target: 0 }
+    ]
+}
+
+module.exports.default = function() {
+    const res = http.get('https://rentapps.herokuapp.com/vehicles')
+    const checkRes = k6.check(res, {
+        'status was 200': function(r) {
+            return r.status == 200
+        }
+    })
+
+    if (!checkRes) {
+        myFailRate.add(1)
+    }
+    k6.sleep(1)
+}
